2b0abcca9dd4da8c4f51bb9729c6335d58b6dfdb
Mohammed Wail OURAMDANE <mohammed.wail.our@gmail.com>
2025-12-14 18:58:53 +0100

    Add files via upload

---
+++ Filters/JournalisationFiltre.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TODO_Correction.Filters
+{
+    public class JournalisationFiltre : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string logDir = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            if (!Directory.Exists(logDir))
+                Directory.CreateDirectory(logDir);
+            string filePath = Path.Combine(logDir, "actions.log");
+
+            string date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string user = context.HttpContext.Session.GetString("login") ?? "Anonyme";
+            string controller = context.RouteData.Values["controller"].ToString();
+            string action = context.RouteData.Values["action"].ToString();
+
+
+            string line = $"{date} - {user} - {controller} - {action}{Environment.NewLine}";
+
+            File.AppendAllText(filePath, line);
+        }
+    }
+}
